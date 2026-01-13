a7b4bcdaa0df3228d894d6f5d3f1537fb95e0b1f
shohei <shokoi0618@gmail.com>
2026-01-14 05:16:11 +0900

    ビルドエラーの修復

--- vite.config.ts
+++ vite.config.ts
@@ -47,6 +47,9 @@ export default defineConfig(({ mode }) => {
         },
         devOptions: {
           enabled: true
+        },
+        workbox: {
+          maximumFileSizeToCacheInBytes: 10 * 1024 * 1024 // 10MB
         }
       })
     ],
